1c91e1a381c7e639bd654e8bc3b6a18fa1638eea
fehertoinagyvince1 <vf321@ic.ac.uk>
2023-07-05 16:24:11 +0100

    Update Physics_Emissions_Data.py

--- Physics_Emissions_Data.py
+++ Physics_Emissions_Data.py
@@ -13,13 +13,17 @@ import numpy as np
 
 #Loading Data into a Pandas Data Frames
 
-Data_file_path = r"C:\Users\feher\OneDrive - Imperial College London\UROP 2023\Huxley_Blackett_Data.xlsx"
+Data_file_path = r"C:\Users\feher\OneDrive - Imperial College London\UROP 2023\Blackett_Huxley_Data.xlsx"
 
-Blackett_heating_data = pd.read_excel(Data_file_path, sheet_name= 'BLKT(h)')
-Blackett_electricity_data = pd.read_excel(Data_file_path, sheet_name= 
+
+
+Blackett_heating_data_original = pd.read_excel(Data_file_path, sheet_name= 'BLKT(h)')
+Blackett_electricity_data_original = pd.read_excel(Data_file_path, sheet_name= 
                                           'BLKT(e)')
-Huxley_heating_data = pd.read_excel(Data_file_path, sheet_name= 'HXLY(h)')
-Huxley_electricity_data = pd.read_excel(Data_file_path, sheet_name= 'HXLY(e)')
+Huxley_heating_data_original = pd.read_excel(Data_file_path, sheet_name= 'HXLY(h)')
+Huxley_electricity_data_original = pd.read_excel(Data_file_path, sheet_name= 'HXLY(e)')
+
+
 
 
 
@@ -51,10 +55,86 @@ def set_first_row_as_labels(df):
     return df, df_date
 
 
-Blackett_heating_data, B_h_d_date = set_first_row_as_labels(Blackett_heating_data)
-Blackett_electricity_data, B_e_d_date = set_first_row_as_labels(Blackett_electricity_data)
-Huxley_heating_data, H_h_d_date = set_first_row_as_labels(Huxley_heating_data)
-Huxley_electricity_data, H_e__d_date = set_first_row_as_labels(Huxley_electricity_data) 
+Blackett_heating_data, B_h_d_date = set_first_row_as_labels(Blackett_heating_data_original)
+Blackett_electricity_data, B_e_d_date = set_first_row_as_labels(Blackett_electricity_data_original)
+Huxley_heating_data, H_h_d_date = set_first_row_as_labels(Huxley_heating_data_original)
+Huxley_electricity_data, H_e_d_date = set_first_row_as_labels(Huxley_electricity_data_original) 
+
+#%%
+
+
+#Creating Data drames for the years seperately:
+
+#find 2018 range:
+
+
+
+
+def set_first_row_as_labels_with_years(df):
+    """
+    
+
+    Parameters
+    ----------
+    df : DataFrame
+        Insert the data frame you want to modify.
+
+    Returns
+    -------
+    df : DataFrame
+        Returns a DataFrame that only contains trhe numerical data with the date
+
+    """
+    # Extract the first row as the labels
+    labels = df.iloc[0]
+
+    # Set the labels as the new index
+    df = df[1:]   
+    df.columns = labels
+    
+    df = df.drop(['Site Group', 'Site', 'Monitoring Point', 'Type',
+                  'Unit of Measurement'], axis = 1, errors='ignore')
+    #df = pd.to_datetime(df['Date']).dt.date
+    return df
+
+def separating_years(df):
+    """
+    
+
+    Parameters
+    ----------
+    df : DataFrame
+        The Dataframe you want to modify.
+
+    Returns
+    -------
+    separate_dfs : list
+        Returns a list of DataFrames each corresponding to a calender year.
+
+    """
+    B_h_d_wyears = set_first_row_as_labels_with_years(df)
+    
+    B_h_d_wyears['Date'] = pd.to_datetime(B_h_d_wyears['Date'])
+    
+    
+    # Group the DataFrame based on the 'Condition' column
+    desired_years = [2018,2019, 2020, 2021,2022]
+    
+    # Create separate DataFrames for each group
+    separate_dfs = []
+    for year in desired_years:
+        year_df = B_h_d_wyears[B_h_d_wyears['Date'].dt.year == year]
+        
+        #year_df.drop(['Date'], axis = 1)
+        separate_dfs.append(year_df.drop(['Date'], axis =1))
+    return separate_dfs
+
+B_h_d_separated = separating_years(Blackett_heating_data_original)
+B_e_d_separated = separating_years(Blackett_electricity_data_original)
+H_e_d_separated = separating_years(Huxley_electricity_data_original)
+H_h_d_separated = separating_years(Huxley_heating_data_original)
+
+
 
 
 
@@ -85,7 +165,9 @@ H_e_d_column_name = Huxley_electricity_data.columns
 
 time_column_name = thirty_min_sample.columns 
 
-print(Blackett_heating_data[0])
+max_B_h_d = Blackett_heating_data.max()
+
+
 
 #%%
 
@@ -126,24 +208,65 @@ ax2.set_xlabel('Date', fontsize = 13)
 plt.show()
 #%%
 
-    
+fig,ax=plt.subplots()   
 for x_col, y_col in zip( time_column_name, H_h_d_column_name):
-    plt.plot(thirty_min_sample[x_col],Huxley_heating_data[y_col], 'o',
-             label=f'{x_col} vs {y_col}')
-plt.title('Huxley Hheating', fontsize = 15)
-plt.xlabel('Time elapsed from start (Days)')
-plt.ylabel('Total Heat Energy (kwH)')
+    ax.plot(thirty_min_sample[x_col],Huxley_heating_data[y_col], 'o',
+             label=f'{x_col} vs {y_col}', color = 'Blue')
+ax.set_title('Huxley Heating', fontsize = 15)
+ax.set_xlabel('Time elapsed from start (Days)', fontsize = 13)
+ax.set_ylabel('Total Heat Energy (kwH)', fontsize = 13)
+ax2 = ax.twiny()
+ax2.tick_params(axis='x', which='both', bottom=False, top=True)
+ax2.plot(H_h_d_date, Huxley_heating_data[0], 'o', color = 'blue')
+tick_positions = [date for date in H_h_d_date if (date.month == 1 and date.day == 1)]
+ax2.set_xticks(tick_positions)
+ax2.set_xlabel('Date', fontsize = 13)
+
+
 plt.show()
+#%%
+fig,ax=plt.subplots()
     
 for x_col, y_col in zip(time_column_name, H_e_d_column_name):
-    plt.plot(thirty_min_sample[x_col],Huxley_electricity_data[y_col], 'o',
-             label=f'{x_col} vs {y_col}')
-plt.title('Huxley Electricity', fontsize = 15)
-plt.xlabel('Time elapsed from start (Days)')
-plt.ylabel('Total Electrical Energy (kwH)')
+    ax.plot(thirty_min_sample[x_col],Huxley_electricity_data[y_col], '.',
+             label=f'{x_col} vs {y_col}', color = 'purple')
+ax.set_title('Huxley Electricity', fontsize = 15)
+ax.set_xlabel('Time elapsed from start (Days)',  fontsize = 13 )
+ax.set_ylabel('Total Electrical Energy (kwH)',  fontsize = 13)
+ax2 = ax.twiny()
+ax2.tick_params(axis='x', which='both', bottom=False, top=True)
+ax2.plot(H_h_d_date, Huxley_electricity_data[0], '.', color = 'purple')
+tick_positions = [date for date in H_e_d_date if (date.month == 1 and date.day == 1)]
+ax2.set_xticks(tick_positions)
+ax2.set_xlabel('Date', fontsize = 13)
 plt.show()
 
 
+#%%
+#The aim of this next section is to do all these plots for each individual year
+
+
+fig,ax=plt.subplots()
+
+for x_col, y_col in zip(time_column_name,B_h_d_column_name):
+    if x_col <= 365:
+        ax.plot(thirty_min_sample[x_col],Blackett_heating_data[y_col], 'o',
+             label=f'{x_col} vs {y_col}', color = "green")
+    else:
+        break
+ax.set_title('Blackett Heating', fontsize = 15)
+ax.set_xlabel('Time elapsed from start (Days)', fontsize = 13)
+ax.set_ylabel('Total Heat Energy (kwH)', fontsize = 13)
+#%%
+ax2 = ax.twiny()
+ax2.tick_params(axis='x', which='both', bottom=False, top=True)
+ax2.plot(B_h_d_date, Blackett_heating_data[0],'o', color = 'green')
+tick_positions = [date for date in B_h_d_date if (date.month == 1 and date.day == 1)]
+ax2.set_xticks(tick_positions)
+ax2.set_xlabel('Date', fontsize = 13)
+#
+
+plt.show()
 
 
 
